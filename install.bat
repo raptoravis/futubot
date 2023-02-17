b22e05aeb632aeff2b2d07f2b7a1ba026719a40f
liyonggang <liyonggang@ztgame.com>
2023-02-17 10:56:41 +0800

    add bat

---
+++ install.bat
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+
+rem "-v" for verbose option.
+rem "-e" means installing the project in editable mode,
+rem so any local modifications on the project can be used without reinstalling it.
+pip install -v -e .
---
+++ run.bat
@@ -0,0 +1,2 @@
+python demo/app.py demo/configs/futubot_config_demo.py --display-all-cols
+rem python demo/main.py demo/configs/futubot_config_demo.py --display-all-cols
